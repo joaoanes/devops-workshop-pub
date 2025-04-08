2f570514de63af58401a7440c2107760929cc8c2
João Anes <hi@joaoanes.website>
2025-04-08 17:01:09 +0100

    Add initial slides

---
+++ slides/components/VClickList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-click v-for="(item, index) in processedItems" :key="index">
+      <component :is="item" />
+    </v-click>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    processedItems() {
+      const slotContent = this.$slots.default ? this.$slots.default() : [];
+      return slotContent.map(node => {
+
+        if (node.type === 'li' && node.children) {
+          return node
+        }
+      })
+    }
+  }
+}
+</script>
